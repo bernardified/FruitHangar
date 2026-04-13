33870e94ece222dfbd9c202959978998b8db1735
Bernard Koh <bernardified@gmail.com>
2026-04-13 14:34:10 +0800

    prepared for deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,14 +4,25 @@ import cors from 'cors'
 import fruitsRoutes from "./routes/fruitsRoutes.js"
 import ordersRoutes from "./routes/ordersRoutes.js"
 import { connectDB } from './config/db.js'
+import path from "path"
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 const app = express()
 dotenv.config()
 
-app.use(cors({origin:"http://localhost:5173"}))
+if(process.env.NODE_ENV !== "production") {
+    app.use(cors({origin:"http://localhost:5173"}))
+}
 app.use(express.json()) 
 app.use("/api/fruits", fruitsRoutes)
 app.use("/api/orders", ordersRoutes)
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../../frontend/dist")))
+}
 
 const PORT = process.env.PORT
 connectDB().then(() => {
--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,4 +1,4 @@
-export function formatDate (date) {
+export function formatDate (date:any) {
     return date.toLocaleDateString(
         "en-US",
         {
